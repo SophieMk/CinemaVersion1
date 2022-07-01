8208409867bd6d326acbfc00b1723d54da08d9a0
anchi <andrey@cherkashin.su>
2022-07-02 00:55:27 +0300

    Delited user_name field

--- cinema/authorization/models.py
+++ cinema/authorization/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class User(models.Model):
     login = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-    user_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     ava = models.ImageField(upload_to='avatar')
     id_user = models.IntegerField(null=False)
