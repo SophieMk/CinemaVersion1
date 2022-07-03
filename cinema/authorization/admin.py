806c596085636cb84caea72e2509dbf67d237993
anchi <andrey@cherkashin.su>
2022-07-03 15:06:28 +0300

    Added home page urls Created base.html Added home page added player page

--- cinema/authorization/admin.py
+++ cinema/authorization/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import User
 
 @admin.register(User)
+
 class UserAdmin(admin.ModelAdmin):
     pass
--- cinema/movie/views.py
+++ cinema/movie/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .models import Movie
 
 # Create your views here.
+
+
 def allFilms(request):
    movies = Movie.objects.all()
-   return render(request,'ListOfFilms.html',{'movies':movies})
+   return render(request, 'ListOfFilms.html', {'movies': movies})
+
+def player(request, film_id):
+   return render(request, 'player.html', {'film_id': film_id})
+
