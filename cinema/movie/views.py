2505edb353209e199b57e39f2977a91b45fd5ee4
anchi <andrey@cherkashin.su>
2022-07-04 16:26:29 +0300

    Added home page urls Created base.html Added home page added player page Updated css files

--- cinema/movie/views.py
+++ cinema/movie/views.py
@@ -1,3 +1,4 @@
+import io
 from django.shortcuts import render
 from .models import Movie
 from .torrent import make_magnet_from_file
@@ -14,5 +15,9 @@ def all_films(request):
 def player(request, film_id):
    film = Movie.objects.get(movieId=film_id)
    magnet = make_magnet_from_file(film_id)
-   return render(request, 'player.html', {'film': film, 'magnet': magnet})
+   print("Default buffer size:", io.DEFAULT_BUFFER_SIZE)
+   file = open(film.torrent.path, mode="r", buffering=5)
+   print(file.line_buffering)
+   file_contents = file.buffer
+   return render(request, 'player.html', {'film': film, 'magnet': magnet, 'torrentBuf': file_contents})
 
