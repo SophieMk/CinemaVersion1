6d0659f37b901b67d329290b54fe40bce47c1513
Skiff <dogfox02@gmail.com>
2022-07-04 16:27:23 +0300

    Home url added

--- cinema/movie/models.py
+++ cinema/movie/models.py
@@ -4,12 +4,21 @@ from django.db import models
 
 
 class Movie(models.Model):
+
+    TYPES = {
+        (1, 'Anime'),
+        (2, 'Films'),
+        (3, 'Serials')
+    }
+
     name = models.CharField(max_length=40)
     link = models.CharField(max_length=200)
     image = models.ImageField(upload_to='movie')
     rate = models.IntegerField(null=False)
     movieId = models.IntegerField(null=False)
     torrent = models.FileField(upload_to='torrent', null=True)
+    type = models.PositiveSmallIntegerField( ("type"), choices=TYPES)
+
 
     class Meta:
         verbose_name = 'Фильм'
