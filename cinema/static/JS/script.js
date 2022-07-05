dc7a18f9127a499b47e5ba1f0efd3f26e4e0cb33
Danya <alaguzero@gmail.com>
2022-07-05 14:56:28 +0300

    added search-bar

--- cinema/static/JS/script.js
+++ cinema/static/JS/script.js
@@ -4,6 +4,18 @@ let rightButton = document.querySelector(".right-button");
 let slides = document.querySelectorAll(".carousel-item");
 let timerID;
 
+function OpenClose_SearchBar(search_bar) {
+    bar = document.getElementById('search_bar');
+    bar.classList.toggle('hide');
+    bar.classList.toggle('show');
+}
+
+function ShowHide_Menu(menu_without_search) {
+    menu = document.getElementById('menu_without_search');
+    menu.classList.toggle('show_menu');
+    menu.classList.toggle('hide_menu');
+}
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
