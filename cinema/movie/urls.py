78f6813307ae1c00856dee99359dcf85fbc3c630
anchi <andrey@cherkashin.su>
2022-07-09 15:19:38 +0300

    Added home page urls Created base.html Added home page added player page Updated css files torrent player was done added redirecting to register added rating and rating form added log_in urls

--- cinema/movie/urls.py
+++ cinema/movie/urls.py
@@ -17,13 +17,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .views import all_films, all_animes, all_serials, all_things, player
-
+from .views import all_films, all_animes, all_serials, all_things, player, all_things_logged_in, all_animes_logged_in, \
+    all_serials_logged_in, all_films_logged_in, player_logged_in
 
 urlpatterns = [
     path('look', all_things),
     path('animes', all_animes),
     path('serials', all_serials),
     path('films', all_films),
-    path('movie/<int:film_id>', player)
+    path('movie/<int:film_id>', player),
+    path('<int:user_id>/look', all_things_logged_in),
+    path('<int:user_id>/animes', all_animes_logged_in),
+    path('<int:user_id>/serials', all_serials_logged_in),
+    path('<int:user_id>/films', all_films_logged_in),
+    path('<int:user_id>/movie/<int:film_id>', player_logged_in)
 ]
--- cinema/movie/views.py
+++ cinema/movie/views.py
@@ -1,5 +1,7 @@
 import io
 from django.shortcuts import render
+
+from authorization.models import User
 from .models import Movie
 from .torrent import make_magnet_from_file
 from django.core.files import File
@@ -8,23 +10,50 @@ from django.core.files import File
 
 def all_things(request):
    movies = Movie.objects.all()
-   return render(request, 'ListOfFilms.html', {'movies': movies})
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'logged_in': False})
 
 def all_films(request):
    movies = Movie.objects.filter(type = 2)
-   return render(request, 'ListOfFilms.html', {'movies': movies})
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'logged_in': False})
 
 def all_animes(request):
    movies = Movie.objects.filter(type = 1)
-   return render(request, 'ListOfFilms.html', {'movies': movies})
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'logged_in': False})
 
 def all_serials(request):
    movies = Movie.objects.filter(type = 3)
-   return render(request, 'ListOfFilms.html', {'movies': movies})
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'logged_in': False})
 
 
 def player(request, film_id):
    film = Movie.objects.get(movieId=film_id)
    magnet = make_magnet_from_file(film_id)
-   return render(request, 'player.html', {'film': film, 'magnet': magnet})
+   return render(request, 'player.html', {'film': film, 'magnet': magnet, 'logged_in': False})
+
+def all_things_logged_in(request, user_id):
+   user = User.objects.get(id=user_id)
+   movies = Movie.objects.all()
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'user': user, 'logged_in': True})
+
+def all_films_logged_in(request, user_id):
+   user = User.objects.get(id=user_id)
+   movies = Movie.objects.filter(type = 2)
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'user': user, 'logged_in': True})
+
+def all_animes_logged_in(request, user_id):
+   user = User.objects.get(id=user_id)
+   movies = Movie.objects.filter(type = 1)
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'user': user, 'logged_in': True})
+
+def all_serials_logged_in(request, user_id):
+   user = User.objects.get(id=user_id)
+   movies = Movie.objects.filter(type = 3)
+   return render(request, 'ListOfFilms.html', {'movies': movies, 'user': user, 'logged_in': True})
+
+
+def player_logged_in(request, film_id, user_id):
+   user = User.objects.get(id=user_id)
+   film = Movie.objects.get(movieId=film_id)
+   magnet = make_magnet_from_file(film_id)
+   return render(request, 'player.html', {'film': film, 'user': user, 'logged_in': True, 'magnet': magnet})
 
